f0c50a18c3ecfe2c25f2f52773ad2a340fdcc79d
progaurav052 <gpprojects511@gmail.com>
2026-04-21 12:39:15 +0530

    new Project Initilizated : DMA Programming from Scratch using CMSIS Headers , NO HAL Drivers used in this Project

---
+++ M2P_USART2_DMA_SCRATCH/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/core_cm4.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_version.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_compiler.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_gcc.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/mpu_armv7.h \
+ /Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/core_cm4.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_version.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_compiler.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/cmsis_gcc.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Include/mpu_armv7.h:
+/Users/ggpai/Desktop/DMA/workspace/M2P_USART2_DMA_SCRATCH/CMSIS_CORE/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
